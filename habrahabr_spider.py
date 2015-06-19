6ec5e357331c20127fa07797ccc0441f857d1f98
Peter Romov <peter@romov.ru>
2015-06-19 16:50:01 +0300

    spider modified

--- habrahabr_spider.py
+++ habrahabr_spider.py
@@ -1,28 +1,13 @@
 # -*- coding: utf-8 -*
 
 import datetime
+import urlparse
+import dateutil.parser
 
 import scrapy
 import scrapy.crawler
 import scrapy.settings
-import dateutil.parser
 
-
-class PostItem(scrapy.Item):
-    title = scrapy.Field()
-    published = scrapy.Field()
-    hubs = scrapy.Field()
-    content = scrapy.Field()
-    tags = scrapy.Field()
-    pageviews = scrapy.Field()
-    favs_count = scrapy.Field()
-    author = scrapy.Field()
-    author_url = scrapy.Field()
-    author_rating = scrapy.Field()
-    comments_count = scrapy.Field()
-    comments = scrapy.Field()
-
-    last_update = scrapy.Field()
 
 
 def parse_habrahabr_datetime(date_str):
@@ -49,67 +34,96 @@ def parse_habrahabr_datetime(date_str):
     return dateutil.parser.parse(s)
 
 
-class HabrahabrSpider(scrapy.Spider):
-    name = 'habrahabr'
+class TMSpider(scrapy.Spider):
+    name = 'tm'
+    allowed_domains = ['habrahabr.ru', 'megamozg.ru', 'geektimes.ru', 'tmtmfeed.ru']
 
-    def __init__(self, posts_range=None, *args, **kwargs):
+    tm_sites = ['http://habrahabr.ru', 'http://megamozg.ru', 'http://geektimes.ru']
+    feed_pages = ['/interesting/', '/all/', '/top/']
+
+    def __init__(self, post_ids=None, *args, **kwargs):
         super(HabrahabrSpider, self).__init__(*args, **kwargs)
-        self.posts_range = eval(posts_range)
+
+        if post_ids is not None:
+            if isinstance(post_ids, str):
+                post_ids = eval(post_ids)
+            self.post_ids = post_ids
 
     def start_requests(self):
-        for post_id in self.posts_range:
-            yield scrapy.Request('http://habrahabr.ru/post/%d/' % post_id, callback=self.parse_post)
+        yield Request('http://tmfeed.ru/', self.parse_tmfeed)
+
+        for site in self.tm_sites:
+            for page in self.feed_pages:
+                url = urlparse.urljoin(site, page)
+                yield scrapy.Request(url, self.parse_feed)
+
+        if self.post_ids:
+            for post_id in self.post_ids:
+                yield scrapy.Request('http://habrahabr.ru/post/%d/' % post_id, self.parse_post)
 
     def parse_post(self, response):
+        for user_url in commenter_urls:
+            yield Request(user_url, self.parse_user)
+
+        last_update = datetime.datetime.now()
         page_title = response.xpath('//html/head/title/text()').extract_first()
 
-        FORBID_TITLES = [
-            u'Geektimes \u2014 \u0414\u043e\u0441\u0442\u0443\u043f \u043a \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0435 \u043e\u0433\u0440\u0430\u043d\u0438\u0447\u0435\u043d',
-            u'\u0425\u0430\u0431\u0440\u0430\u0445\u0430\u0431\u0440 \u2014 \u0414\u043e\u0441\u0442\u0443\u043f \u043a \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0435 \u043e\u0433\u0440\u0430\u043d\u0438\u0447\u0435\u043d',
-        ]
-
-        if page_title in FORBID_TITLES:
-            return
-
-        post = PostItem()
-
         post_sel = response.css('.post')
-        post['title'] = post_sel.css('h1.title span.post_title ::text').extract_first()
-        post['published'] = parse_habrahabr_datetime(post_sel.css('div.published ::text').extract_first())
 
+        post_id = post_sel.css('::attr(id)')
+        if post_id.startswith('post_'):
+            post_id = post_id[len('post_'):]
+        else:
+            raise RuntimeError('Bad post_id: "%s"' % post_id)
+
+        post_title = post_sel.css('h1.title span.post_title ::text').extract_first()
+        post_published = parse_habrahabr_datetime(post_sel.css('div.published ::text').extract_first())
+
+        # flags (tutorial, sandbox, etc.)
+        flags = []
+        flag_classes = ' '.join(post_sel.css('h1.title .flag ::attr(class)').extract()).split(' ')
+        for flag_class in flag_classes:
+            if flag_class.startswith('flag_'):
+                flags.append(flag_class[len('flag_'):])
+
+        # hubs
         hubs_sel = post_sel.css('.hubs a.hub')
         hubs = []
         for hub_sel in hubs_sel:
             hub_name = hub_sel.xpath('text()').extract_first()
             hub_url = hub_sel.xpath('@href').extract_first()
             hubs.append((hub_name, hub_url))
-        post['hubs'] = hubs
+            yield Response(hub_url, self.parse_feed)
 
-        post['content'] = post_sel.css('div.content').extract_first()
-        post['tags'] = post_sel.css('ul.tags li a ::text').extract()
+        content_html = post_sel.css('div.content').extract_first()
+        tags = post_sel.css('ul.tags li a ::text').extract()
 
         infopanel_sel = post_sel.css('div.infopanel')[0]
-        post['pageviews'] = int(infopanel_sel.css('div.pageviews ::text').extract_first())
+        pageviews = int(infopanel_sel.css('div.pageviews ::text').extract_first())
         favs_count = infopanel_sel.css('div.favs_count ::text').extract_first()
-        post['favs_count'] = int(favs_count) if favs_count is not None else 0
+        favs_count = int(favs_count) if favs_count is not None else 0
 
         author_sel = infopanel_sel.css('div.author')
-        post['author'] = author_sel.css('a ::text').extract_first()
-        post['author_url'] = author_sel.css('a ::attr(href)').extract_first()
-        post['author_rating'] = float(author_sel.css('span.rating ::text').extract_first().replace(u'\u2013', '-').replace(',', '.'))
+        author_name = author_sel.css('a ::text').extract_first()
+        author_url = author_sel.css('a ::attr(href)').extract_first()
+        author_rating = float(author_sel.css('span.rating ::text').extract_first().replace(u'\u2013', '-').replace(',', '.'))
+
+        yield Request(author_url, self.parse_user)
 
         comments_sel = response.css('#comments')
-        post['comments_count'] = comments_sel.css('h2.title span#comments_count ::text').extract_first()
+        comments_count = comments_sel.css('h2.title span#comments_count ::text').extract_first()
 
         def extract_comments(sel):
             childs_sel = sel.xpath('./div[@class="comment_item"]')
 
             extracted_comments = []
+            commenter_urls = set()
 
             for child_sel in childs_sel:
                 body_sel = child_sel.xpath('./div[@class="comment_body "]')[0]
                 reply_sel = child_sel.xpath('./div[@class="reply_comments"]')[0]
-                replies = extract_comments(reply_sel)
+                replies, child_commenter_urls = extract_comments(reply_sel)
+                commenter_urls.update(child_commenter_urls)
 
                 if len(body_sel.css('div.author_banned')) > 0:
                     comment = {
@@ -120,6 +134,7 @@ class HabrahabrSpider(scrapy.Spider):
                     time = body_sel.css('time ::text').extract_first()
                     time = parse_habrahabr_datetime(time)
                     username = body_sel.css('a.username ::text').extract_first()
+                    user_url = body_sel.css('a.username ::attr(href)').extract_first()
                     link_to_comment = body_sel.css('a.link_to_comment ::attr(href)').extract_first()
                     votes = int(body_sel.css('div.voting span.score ::text').extract_first().replace(u'\u2013', '-'))
                     message_html = body_sel.css('div.message').extract_first()
@@ -133,19 +148,83 @@ class HabrahabrSpider(scrapy.Spider):
                         'message_html': message_html,
                         'replies': replies,
                     }
+                    commenter_urls.add(user_url)
 
                 extracted_comments.append(comment)
 
-            return extracted_comments
+            return extracted_comments, commenter_urls
 
-        post['comments'] = extract_comments(comments_sel)
-        post['last_update'] = datetime.datetime.now()
+        comments, commenter_urls = extract_comments(comments_sel)
 
-        yield post
+        yield {
+            '_type': 'post',
+            '_id': post_id,
+            'title': post_title,
+            'published': post_published,
+            'flags': flags,
+            'hubs': hubs,
+            'content_html': content_html,
+            'tags': tags,
+            'pageviews': pageviews,
+            'favs_count': favs_count,
+            'author': author_name,
+            'author_rating': author_rating,
+            'comments_count': comments_count,
+            'comments': comments,
+            'last_update': last_update,
+        }
+
+        for entry in self.parse_page_links(response):
+            yield entry
 
 
     def parse_user(self, response):
-        raise NotImplementedError()
+        if 0:
+            yield {
+                '_type': 'user',
+
+            }
+
+    def parse_user_feed(self, response):
+        return
+
+    def parse_feed(self, response):
+        feed_post_urls = response.css('div.posts div.post h1.title a.post_title ::attr(href)').extract()
+        for post_url in feed_post_urls:
+            post_url = urlparse.urljoin(response.url, post_url)
+            yield Request(post_url, self.parse_post)
+
+        nav_urls = response.css('div.page-nav ul#nav-pages li a ::attr(href)').extract()
+        for page_url in nav_urls:
+            page_url = urlparse.urljoin(response.url, page_url)
+            yield Request(page_url, self.parse_feed)
+
+        for entry in self.parse_page_links(response):
+            yield entry
+
+    def parse_user_rating(self, response):
+        sel_users = response.css('div#peoples div.user div.info div.userlogin div.username')
+        for sel_user in sel_users:
+            user_name = sel_user.css('::text').extract_first()
+            user_url = sel_user.css('::attr(href)').extract_first()
+            user_url = urlparse.urljoin(response.url, user_url)
+            yield Request(user_url, self.parse_user)
+
+        nav_urls = response.css('div.page-nav ul#nav-pages li a ::attr(href)').extract()
+        for page_url in nav_urls:
+            page_url = urlparse.urljoin(response.url, page_url)
+            yield Request(page_url, self.parse_user_rating)
+
+    def parse_page_links(self, response):
+        block_post_urls = response.css('div.post_item a.post_name ::attr(href)').extract()
+        for post_url in block_post_urls:
+            post_url = urlparse.urljoin(response.url, post_url)
+            yield Request(post_url, self.parse_post)
+
+        block_hub_urls = response.css('ul.categories li a ::attr(href)').extract()
+        for hub_url in block_hub_urls:
+            hub_url = urlparse.urljoin(response.url, hub_url)
+            yield Request(hub_url, self.parse_feed)
 
 
 
